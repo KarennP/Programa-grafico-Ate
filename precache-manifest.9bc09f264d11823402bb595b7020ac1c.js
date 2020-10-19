922b23a34e19969eefe79f8494492d0f1bf6dcda
Karen <41969662+KarennP@users.noreply.github.com>
2020-10-19 08:24:31 -0500

    Updates

---
+++ precache-manifest.9bc09f264d11823402bb595b7020ac1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a7467c6bfa2992bdde34c51cf2cc16c",
+    "url": "/Programa-grafico-Ate/index.html"
+  },
+  {
+    "revision": "49a8bcb0b80e8027702c",
+    "url": "/Programa-grafico-Ate/static/css/2.c3fe2082.chunk.css"
+  },
+  {
+    "revision": "fdb9560e6f6763f787e1",
+    "url": "/Programa-grafico-Ate/static/css/main.f073bfd5.chunk.css"
+  },
+  {
+    "revision": "49a8bcb0b80e8027702c",
+    "url": "/Programa-grafico-Ate/static/js/2.b125675a.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/Programa-grafico-Ate/static/js/2.b125675a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdb9560e6f6763f787e1",
+    "url": "/Programa-grafico-Ate/static/js/main.6f4853c8.chunk.js"
+  },
+  {
+    "revision": "c5805192cd578ef612f1",
+    "url": "/Programa-grafico-Ate/static/js/runtime-main.fa80e51d.js"
+  }
+]);
